42ee5f148424ba9335ec9a41b7d749e0537701d6
yusra jadoon <zain@simming.ai>
2025-06-01 16:16:32 +0500

    remove frame bug in anayltics

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,5 +1,5 @@
 import { Line } from "react-chartjs-2";
-import analyticsframe from "../assets/Rectangle 1776.png";
+// import analyticsframe from "../assets/Rectangle 1776.png";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -213,13 +213,13 @@ const Analytics = () => {
         </div>
         {/* Graph Section */}
         <div className="relative bg-[#f1f8f4] rounded-2xl p-4 md:p-6 overflow-hidden">
-          {/* Chart Frame as background */}
+          {/* Chart Frame as background
           <img
             src={analyticsframe}
             alt="Analytics Frame"
             className="absolute inset-0 w-full h-full object-cover z-0 pointer-events-none"
             style={{ borderRadius: "1rem" }}
-          />
+          /> */}
           <div className="relative z-10">
             <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-2">
               <div>
